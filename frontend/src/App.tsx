c6a1fb03619bb9fa8272692462773ef472ed6c71
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-06-10 12:10:27 +0300

    Show orgs on app frontpage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,15 +2,22 @@ import { useState, useEffect } from "react";
 import './App.css';
 
 import { get_resources } from "./api/resources";
+import { get_orgs } from "./api/orgs";
 
 import { ResourceList } from "./components/resources";
+import { OrgList } from "./components/orgs";
 
 function App() {
     const [resources, setResources] = useState([]);
+    const [orgs, setOrgs] = useState([]);
 
     useEffect(() => {
         get_resources()
             .then(setResources)
+            .catch(console.error);
+
+        get_orgs()
+            .then(setOrgs)
             .catch(console.error);
     }, [])
 
@@ -18,6 +25,7 @@ function App() {
         <div className="App">
             <h1>Alina</h1>
             <ResourceList resources={resources} />
+            <OrgList orgs={orgs} />
         </div>
     )
 }
