3a9d05955845470ee909c76f80d198b8500319a1
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-06-10 12:54:50 +0300

    Add requests CRUD operations
    Add support for requests. Requires backlinking at org and resource as
    well

---
+++ backend/app/models/request.py
@@ -0,0 +1,46 @@
+from sqlmodel import Field, SQLModel, Relationship
+
+from typing import TYPE_CHECKING, Optional
+
+if TYPE_CHECKING:
+    from app.models.org import Org
+    from app.models.resource import Resource
+
+
+class RequestBase(SQLModel):
+    org_id: int = Field(index=True, foreign_key="org.id")
+    resource_id: int = Field(index=True, foreign_key="resource.id")
+    contact_person: str = Field()
+    contact_email: str = Field()
+    contact_phone: str = Field()
+    event: str = Field()
+    description: str = Field()
+    participants: str = Field()
+    checked: bool = Field()
+
+
+class Request(RequestBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    org: Optional["Org"] = Relationship(back_populates="requests")
+    resource: Optional["Resource"] = Relationship(back_populates="requests")
+
+
+class RequestPublic(RequestBase):
+    id: int
+    org: Optional["Org"]
+    resource: Optional["Resource"]
+
+
+class RequestCreate(RequestBase):
+    pass
+
+
+class RequestUpdate(RequestBase):
+    pass
+
+
+from app.models.org import Org
+from app.models.resource import Resource
+
+_ = Request.model_rebuild()
+_ = RequestPublic.model_rebuild()
