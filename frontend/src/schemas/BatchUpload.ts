b3c3266112ceacb58256b91635ccfa21fab7385a
Janne Salokoski <salokoski.janne@gmail.com>
2025-06-12 09:17:31 +0300

    feat: add schema for BatchUpload

---
+++ frontend/src/schemas/BatchUpload.ts
@@ -0,0 +1,13 @@
+export interface APIRow {
+    org_name: string,
+    alina_dates: string[]
+    sivistys_dates: string[]
+    contact_person: string,
+    contact_email: string,
+    contact_phone: string,
+    event: string,
+    description: string,
+    participants: string,
+    checked: boolean
+}
+
