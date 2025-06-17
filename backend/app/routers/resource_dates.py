30889a954648ab7ed6afffac9be79bf589393561
Janne Salokoski <janne.salokoski@helsinki.fi>
2025-06-17 10:08:15 +0300

    feat(backend): skip empty rows in get_resource_dates
    We do not need the rows that have no dates, so let's skip them

--- backend/app/routers/resource_dates.py
+++ backend/app/routers/resource_dates.py
@@ -26,30 +26,33 @@ async def get_orgs_with_dates_for_resource(resource_id: int, session: SessionDep
     requests = session.exec(query).all()
 
     if not requests:
-        raise HTTPException(status_code=404, detail="No requests found for this resource")
+        raise HTTPException(
+            status_code=404, detail="No requests found for this resource"
+        )
 
     response = []
     for request in requests:
+        if not request.dates:
+            continue
+
         if request.org and request.resource:
             org_resource_date = OrgResourceDate(
-                org_id = request.org.id,
-                org_name = request.org.name,
-                resource_id = request.resource.id,
-                resource_name = request.resource.name,
-                dates = [
+                org_id=request.org.id,
+                org_name=request.org.name,
+                resource_id=request.resource.id,
+                resource_name=request.resource.name,
+                dates=[
                     DatePublic(
-                        id = date.id,
-                        request_id = date.request_id,
-                        date = date.date,
-                        allocated = date.allocated,
-                        request = None
+                        id=date.id,
+                        request_id=date.request_id,
+                        date=date.date,
+                        allocated=date.allocated,
+                        request=None,
                     )
-
                     for date in request.dates
-                ]
+                ],
             )
 
             response.append(org_resource_date)
 
     return response
-
